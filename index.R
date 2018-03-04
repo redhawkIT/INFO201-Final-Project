e3d6c2dd5f0b5acf8bd783d185e6be9e707eaf80
redhawkIT <khodkodmitry10011983@hotmail.com>
2018-03-04 12:01:23 -0800

    Saving PoC - data from UWSTF returns a data frame that is easily usedgit status

--- index.R
+++ index.R
@@ -1,10 +1,33 @@
 # DEPENDENCIES
-# install.packages('shiny')
-library(shiny)
+
+# install.packages("shiny")
+# install.packages('Hmisc')
+# install.packages('httr')
+# install.packages('jsonlite')
+# install.packages('dplyr')
+# install.packages("tidyr")
+# install.packages("countrycode")
+# install.packages("ggplot2")
+# install.packages("maps")
+# install.packages("sp")
+# install.packages("maptools")
+
+
+# library("shiny")
+library(Hmisc)
+library(httr)
+library(jsonlite)
+library(dplyr)
+library(tidyr)
+# library("countrycode")
+# library("ggplot2")
+# library("maps")
+# library("sp")
+# library("maptools")
 
 # IMPORTS
-relative.path <- '~info201/INFO201-Final-Project'
-setwd(relative.path)
+# relative.path <- '~info201/INFO201-Final-Project'
+# setwd(relative.path)
 
 # emissions.data <- read.csv(
 #   './data/WDI_emissions_Data.csv',
@@ -12,8 +35,25 @@ setwd(relative.path)
 #   stringsAsFactors = FALSE
 # )
 
-source('./ui.R')
-source('./server.R')
+# source('./ui.R')
+# source('./server.R')
+#
+# # START APPLICATION
+# shinyApp(ui = the.ui, server = the.server)
 
-# START APPLICATION
-shinyApp(ui = the.ui, server = the.server)
+
+# EX: "https://api.propublica.org/congress/v1/115/bills/hr21.json"
+# https://uwstf.org/v2/proposal/?query={"year":"<2018"}&populate=["body"]&select=["title","year","number","organization","category","asked","received","body"]
+stf.api <- "https://uwstf.org/v2"
+model <- 'proposal'
+query <- '"year":"<2018"'
+populate <- '"body"'
+
+resource <- sprintf('/%s/?query={%s}&populate=[%s]', model, query, populate)
+uri <- paste0(stf.api, resource)
+print(uri)
+
+response <- GET(uri)
+print(response)
+body <- fromJSON(content(response, "text"))
+print(body)
