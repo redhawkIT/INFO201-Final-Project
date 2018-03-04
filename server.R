f2fb911bb1419a86d601da42ab906002ac0cc55e
Daniel-Lu <dlusion@uw.edu>
2018-03-04 15:26:16 -0800

    continued work on ui and server

--- server.R
+++ server.R
@@ -11,27 +11,13 @@ library("tidyr")
 #data <- 
 
 the.server <- function(input, output) {
-
-  placeholder1 <- reactive({
-
-    positivity <- input$range
-
-    year.filter <- input$years
-
-    categories <- input$category
-
-    filtered.data <- data
-
-    return()
-
-  })
   
   output$ui <- renderUI({
-    switch (
-              input$tab,
+    switch(input$tab,
               "graph" = NULL,
               "table" = checkboxGroupInput("years", label = h3("Which years to include?"), 
-                                           choices = list("2014" = 2014, "2015" = 2015, "2016" = 2016, "2017" = 2017),
+                                           choices = list("2014" = 2014, "2015" = 2015, "2016" = 2016, 
+                                                          "2017" = 2017),
                                            selected = 2014
               ),
               "summary" =  sliderInput("range", 
@@ -39,6 +25,20 @@ the.server <- function(input, output) {
                                        min = 2014, max = 2017, value = c(2014, 2017), sep = ''
               )
           )
+  })
+  
+  placeholder1 <- reactive({
+    
+    positivity <- input$range
+    
+    year.filter <- input$years
+    
+    categories <- input$category
+    
+    filtered.data <- data
+    
+    return()
+    
   })
   
   output$table <- renderTable({
@@ -53,18 +53,28 @@ the.server <- function(input, output) {
 
   })
 
-  output$hist <- renderPlot({
+  output$graph <- renderPlot({
 
     dist <- input$dist
 
     n <- input$n
 
     hist(placeholder2(),
-
          main = "Title of Histogram",
-
          col = "#75AADB", border = "white")
 
+  })
+  
+  placeholder3 <- reactive({
+    
+    return()
+    
+  })
+  
+  output$summary <- renderTable({
+    
+    placeholder3
+    
   })
 
 }
