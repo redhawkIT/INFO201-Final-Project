a6d53f7a4f650a5967e4dcf682e3f3a71fc497a0
Daniel-Lu <dlusion@uw.edu>
2018-03-07 12:13:40 -0800

    Sick people stuff, partially reverting changes

--- ui.R
+++ ui.R
@@ -1,8 +1,9 @@
+minVal <- group_by(analysis, Year) %>% summarize(mins = min(Valence)) %>% select(mins)
 
 the.ui <- fluidPage(
   # App title ----
   titlePanel("Best AC Group's Final Project"),
-
+  
   # Sidebar layout with input and output definitions ----
   sidebarLayout(
     # Sidebar panel for inputs
@@ -11,13 +12,14 @@ the.ui <- fluidPage(
       sliderInput(
         'range',
         label = h3('Valence Filter'),
-        min = 0,
-        max = 30,
-        value = 15,
+        min = ceiling(max(minVal))[1],
+        max = floor(analysis$Valence[analysis$Valence == max(analysis$Valence)])[1],
+        value = as.numeric(floor((min + max) / 2)),
+        step = 1,
         width = '200%',
         round = FALSE
       ),
-
+      
       checkboxGroupInput(
         'category',
         label = h3('Categories of Request to Include in Analysis'),
@@ -39,7 +41,7 @@ the.ui <- fluidPage(
       uiOutput('ui')
       
     ),
-
+    
     # Main panel for displaying outputs
     mainPanel(
       tabsetPanel(
@@ -48,11 +50,10 @@ the.ui <- fluidPage(
         # Displays a table of 'test', which calls 'filtered()', a reactive filter that
         # creates a table based on the static UI filters.
         #tabPanel('Test', tableOutput('test')),
-        tabPanel('Graphical Analyis', value = 'graph'),
+        tabPanel('Graphical Analyis', plotOutput('graph')),
         tabPanel('Tabular Analysis', tableOutput('table')) ,
         tabPanel('Summary Table', tableOutput('summary'))
       )
     )
   )
 )
-
