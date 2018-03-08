c8b56860f0f2a9dc86ee2a830b728a6d147cd95d
Daniel-Lu <dlusion@uw.edu>
2018-03-08 02:23:29 -0800

    Updated ui and fixed issue in server in which filtered() was not filtering correctly as well as an issue in which endo did not know what to do with an empty table

--- server.R
+++ server.R
@@ -1,4 +1,4 @@
-# Builds 'the.server'
+# Builds the server for our Project's shinyapp
 the.server <- function(input, output) {
   
   #### DEPENDENCIES
@@ -20,6 +20,7 @@ the.server <- function(input, output) {
   ### Visualization:
   # install.packages('ggplot2')
   
+  # Loads the libraries that our shinyapp is dependent on. 
   library(shiny)
   library(shinythemes)
   library(Hmisc)
@@ -47,13 +48,13 @@ the.server <- function(input, output) {
         return(
           analysis %>% filter(
             Valence <= input$range
-            & Category == input$category
+            & Category %in% input$category
           )
         )
       } else if (is.null(input$category) & !is.null(input$year)) {
         analysis %>% filter(
           Valence <= input$range
-          & Year == input$year
+          & Year %in% input$year
         )
       } else {
         return(
@@ -62,28 +63,46 @@ the.server <- function(input, output) {
           )
         )
       }
-    } else
+    } else {
       return(
         analysis %>% filter(
-          Valence <= input$range
-          & Year == input$year
-          & Category == input$category
+          Valence <= input$range, 
+          Year %in% input$year, 
+          Category %in% input$category
         )
       )
+    }
   })
-<<<<<<< HEAD
   
-=======
-
->>>>>>> 09fb14318d3fcc2356178d9c69368c3f379590e3
-  output$ui <- renderUI({
+  output$valence.slider <- renderUI({
+    minVal <- group_by(analysis, Year) %>% summarize(mins = min(Valence)) %>% select(mins)
+    sliderInput(
+      'range',
+      label = h3('Valence Filter'),
+      min = ceiling(max(minVal))[1],
+      max = max(floor(analysis$Valence)),
+      step = 0.5,
+      value = round(max(analysis$Valence)/2), #TODOround((min(analysis$Valence)+max(analysis$Valence))/2), 
+      width = '150%',
+      round = FALSE
+    )
+  })
+  
+  output$categories <- renderUI({
+    checkboxGroupInput(
+      'category',
+      label = h4('Categories of Request to Include in Analysis'),
+      choices = unique(analysis$Category),
+      selected = 'Portable'
+    )
+  })
+  
+  output$dynamic.ui <- renderUI({
     switch(
       input$tab,
       'graph' = NULL,
       'table' = NULL,
-<<<<<<< HEAD
-      'sum.ui' = radioButtons('tab.select', "Select a Summarization:", choices = c("1", "2", "3"),
-                              selected = "1", inline = T)
+      'sum.ui' = NULL
     )
   })
   
@@ -91,17 +110,6 @@ the.server <- function(input, output) {
     filtered() %>% select(Title, Organization, Asked, Received, Anticipation, Trust, Joy, Valence)
   })
   
-=======
-      'sum.ui' = radioButtons('tab.select', "Select a Summarization:", choices = c("1", "2", "3", "4"),
-                              selected = "1", inline = T)
-    )
-  })
-
-  output$table <- renderDataTable({
-    filtered() %>% select(Title, Organization, Asked, Received, Anticipation, Trust, Joy, Valence)
-  })
-
->>>>>>> 09fb14318d3fcc2356178d9c69368c3f379590e3
   output$graph <- renderPlot({
     filtered.table <- filtered()
     emotions <- select(filtered.table, Anger, Anticipation, Disgust, Fear, Sadness, Surprise, Trust)
@@ -113,17 +121,6 @@ the.server <- function(input, output) {
             names.arg = colnames(emotions),
             col = c("red", "orange", "yellow", "green",
                     "blue", "purple", "black"))
-<<<<<<< HEAD
-=======
-  })
-  
-  output$scatter <- renderPlot({
-    ggplot(data = filtered()) +
-      geom_point(mapping = aes(x = Valence, y = Received), color = "red") +
-      labs(title = "Valence versus Money Received",
-           x = "Valence",
-           y = "Money Received in Dollars")
->>>>>>> 09fb14318d3fcc2356178d9c69368c3f379590e3
   })
   
   output$scatter <- renderPlot({
@@ -134,11 +131,19 @@ the.server <- function(input, output) {
            y = "Money Received in Dollars")
   })
   
-  
+  # Creates a reactive function called 'sum.data' that returns a list. List contains all data required in the
+  # summary visualizations. 
   sum.data <- reactive({
+    
+    # Groups data from reactive function filtered() by their category, and then uses this, along with summary
+    # tools, to create data frame containing methods of central tendency and variability. 
     category.sums <- group_by(filtered(), Category) %>% 
       summarize(Avg.amount.asked = mean(Asked), Avg.amount.received = mean(Received),
                 Max.received = max(Received), Median.recieved = median(Received), Standard.dev = sd(Received))
+    
+    # Groups data from filtered() by Valence, grouping Valence into several buckets. Assigns each bucket 
+    # a label and then uses summary functions to create data frame containing methods of central tendency
+    # and variability.
     valence.sums <- mutate(filtered(), Valence.Group = cut(filtered()$Valence,
                                                            breaks = c(0, 5, 10, 15, 20, 25, 30, 35, 40),
                                                            labels = c("0 - 5", "5 - 10", "10 - 15", "15 - 20",
@@ -147,33 +152,42 @@ the.server <- function(input, output) {
     valence.sums <- group_by(valence.sums, Valence.Group) %>% 
       summarize(Avg.amount.asked = mean(Asked), Avg.amount.received = mean(Received),
                 Max.received = max(Received), Median.recieved = median(Received), Standard.dev = sd(Received))
-    endorsement.sums <- filtered() %>% 
-      mutate(Endorsements = unlist(filtered()$Endorsements, use.names = FALSE))
-    endorsement.sums <- mutate(endorsement.sums, Endorsement.group = cut(endorsement.sums$Endorsements,
-                                                                         breaks = c(-1, 5, 10, 20,
-                                                                                    30, 45, 80, 100, 200), 
-                                                                         labels = c("-1 - 5", "5 - 10",
-                                                                                    "10 - 20", "20 - 30",
-                                                                                    "30 - 45", "45 - 80", 
-                                                                                    "80 - 100", "100 - 200")))
-    endorsement.sums <- group_by(endorsement.sums, Endorsement.group) %>% 
-      summarize(Avg.amount.asked = mean(Asked), Avg.amount.received = mean(Received),
-                Max.received = max(Received), Median.recieved = median(Received), Standard.dev = sd(Received))
+    
+    # Determines if filtered is empty or not. If so, returns empty data frame. Otherwise, unlists Endorsements 
+    # into a vector of numbers and groups data from filtered() Endorsement numbers. Uses this to create a
+    # data frame with methods of central tendency and variability.
+    endorsement.sums <- filtered()
+    if (nrow(filtered()) >= 1) {
+      endorsement.sums <- mutate(endorsement.sums, Endorsements = unlist(filtered()$Endorsements,
+                                                                         use.names = FALSE))
+      endorsement.sums <- mutate(endorsement.sums, Endorsement.group = cut(endorsement.sums$Endorsements,
+                                                                           breaks = c(-1, 5, 10, 20,
+                                                                                      30, 45, 80, 100, 200),
+                                                                           labels = c("-1 - 5", "5 - 10",
+                                                                                      "10 - 20", "20 - 30",
+                                                                                      "30 - 45", "45 - 80",
+                                                                                      "80 - 100", "100 - 200")))
+      endorsement.sums <- group_by(endorsement.sums, Endorsement.group) %>%
+        summarize(Avg.amount.asked = mean(Asked), Avg.amount.received = mean(Received),
+                  Max.received = max(Received), Median.recieved = median(Received), Standard.dev = sd(Received))
+    } 
     return(list(cat = category.sums, val = valence.sums, endo = endorsement.sums))
   })
   
-  output$summary <- renderTable({
-    if(input$tab.select == '1') {
-      sum.data()$cat
-    } else if(input$tab.select == '2') {
-      sum.data()$val
-    } else {
-      sum.data()$endo
-    }
+  # Outputs a table containing summary information for requests based on the category that the request falls
+  # into
+  output$cat <- renderTable({
+    sum.data()$cat
   })
-<<<<<<< HEAD
   
-=======
-
->>>>>>> 09fb14318d3fcc2356178d9c69368c3f379590e3
+  # Outputs a table containing summary information for requests based on the valence of the request
+  output$val <- renderTable({
+    sum.data()$val
+  })
+  
+  # Outputs a table containing summary information for requests based on the endorsements that the request had
+  output$endo <- renderTable({
+    sum.data()$endo
+  })
+  
 }
--- ui.R
+++ ui.R
@@ -1,81 +1,31 @@
 source('./client/Overview.R')
 source('./client/Instructions.R')
 
-# BUG: Error in group_by(analysis, Year) : object 'analysis' not found
-minVal <- group_by(analysis, Year) %>% summarize(mins = min(Valence)) %>% select(mins)
-
 the.ui <- fluidPage(
-<<<<<<< HEAD
+  
+  # Sets theme using 'shinythemes'
+  theme = shinytheme('sandstone'),
+  
   # App title ----
   titlePanel('UW Student Technology - Funding Statistics'),
   
   # Sidebar layout with input and output definitions ----
-=======
-
-# Sets theme using 'shinythemes'
-theme = shinytheme('sandstone'),
-
-# App title ----
-titlePanel('UW Student Technology - Funding Statistics'),
-
-# Sidebar layout with input and output definitions ----
->>>>>>> 09fb14318d3fcc2356178d9c69368c3f379590e3
   sidebarLayout(
     # Sidebar panel for inputs
     sidebarPanel(
+      
       # This slider will allow user to filter ouptuded data based on range of sentiments.
-      sliderInput(
-        'range',
-        label = h3('Valence Filter'),
-        min = as.numeric(ceiling(max(minVal))),
-        max = as.numeric(floor(analysis$Valence[analysis$Valence == max(analysis$Valence)]))[1],
-<<<<<<< HEAD
-        step = 1,
-        value = as.numeric(ceiling(max(minVal))), 
-        width = '200%',
-        round = FALSE
-      ),
-      
-      checkboxGroupInput(
-        'category',
-        label = h3('Categories of Request to Include in Analysis'),
-        choices = unique(analysis$Category),
-        selected = 'Portable'
-      ),
-      
-      checkboxGroupInput(
-        'year',
-        label = h3('Which years to include?'),
-        choices = list(
-          '2014' = 2014,
-          '2015' = 2015,
-          '2016' = 2016,
-          '2017' = 2017),
-        selected = '2017'
-      ),
-      
-      uiOutput('ui')
-      
-=======
-        step = 0.5,
-        value = as.numeric(ceiling(max(minVal))), 
-        width = '150%',
-        round = FALSE
-      ),
+      uiOutput('valence.slider'),
       
       hr(),
       
-      uiOutput('ui'),
-  
-      tags$i("If all checkboxes in below controls are unselected, results behave as if all are selected for that metric!"),
-  
-      checkboxGroupInput(
-        'category',
-        label = h4('Categories of Request to Include in Analysis'),
-        choices = unique(analysis$Category),
-        selected = 'Portable'
-      ),
-  
+      uiOutput('dynamic.ui'),
+      
+      tags$i("If all checkboxes in below controls are unselected,
+             results behave as if all are selected for that metric!"),
+      
+      uiOutput('categories'),
+      
       checkboxGroupInput(
         'year',
         label = h4('Which years to include?'),
@@ -85,9 +35,8 @@ titlePanel('UW Student Technology - Funding Statistics'),
           '2016' = 2016,
           '2017' = 2017
         ),
-          selected = '2017'
+        selected = '2017'
       )
->>>>>>> 09fb14318d3fcc2356178d9c69368c3f379590e3
     ),
     
     # Main panel for displaying outputs
@@ -97,13 +46,6 @@ titlePanel('UW Student Technology - Funding Statistics'),
         id = 'tab',
         
         tabPanel('Overview', Overview()),
-<<<<<<< HEAD
-        tabPanel('Instructions', Instructions()),
-        tabPanel('Graphical Analyis', plotOutput('graph')),
-        tabPanel('Scatterplot Analysis', plotOutput('scatter')),
-        tabPanel('Tabular Analysis', dataTableOutput('table')),
-        tabPanel('Summary Table', tableOutput('summary'), value = "sum.ui")
-=======
         # TODO
         #tabPanel('Instructions', Instructions()),
         tabPanel('Graphical Analyis',  tabsetPanel(
@@ -111,8 +53,11 @@ titlePanel('UW Student Technology - Funding Statistics'),
           tabPanel('Scatterplot', plotOutput('scatter'))
         )),
         tabPanel('Tabular Analysis', dataTableOutput('table')),
-        tabPanel('Summary Table' ,tableOutput('summary'), value = "sum.ui")
->>>>>>> 09fb14318d3fcc2356178d9c69368c3f379590e3
+        tabPanel('Summary Table' , tabsetPanel(
+          tabPanel('Category Summary', tableOutput('cat')), 
+          tabPanel('Valence Summary', tableOutput('val')), 
+          tabPanel('Endorsement Summary', tableOutput('endo'))
+        ))
       )
     )
   )
